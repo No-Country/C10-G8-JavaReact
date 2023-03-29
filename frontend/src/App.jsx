0f3f7c608910046d3b796e08052c17f883c93746
Maximiliano <maximartins45@gmail.com>
2023-03-28 21:00:22 -0300

    Actualizando home con los nuevos componentes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import BreakdownOfAccommodation from './pages/BreakdownOfAccommodation'
-import { HashRouter, Routes, Route, Link } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 
 function App() {
