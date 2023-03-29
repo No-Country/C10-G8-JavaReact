09cd47fc61afc2130c95f9921a40cad3cd4e0efc
imnicolas <nicolas.esteban.suarez@hotmail.es>
2023-03-29 12:20:45 -0300

    + Repository and Service package

---
+++ backend/src/main/java/com/backend/Go/Stay/Security/Dto/Mensaje.java
@@ -0,0 +1,21 @@
+
+package com.backend.Go.Stay.Security.Dto;
+
+
+public class Mensaje {
+
+    private String mensaje;
+
+    public Mensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+    
+}
---
+++ backend/src/main/java/com/backend/Go/Stay/Security/Service/UserDetailsServiceImp.java
@@ -0,0 +1,22 @@
+
+package com.backend.Go.Stay.Security.Service;
+
+import com.backend.Go.Stay.Security.Entity.Usuario;
+import com.backend.Go.Stay.Security.Entity.UsuarioPrincipal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImp implements UserDetailsService {
+    @Autowired
+    UsuarioService usuarioService;
+
+    @Override
+    public UserDetails loadUserByUsername(String nombreUsuario) throws UsernameNotFoundException {
+        Usuario usuario = usuarioService.getByNombreUsuario(nombreUsuario).get();
+        return UsuarioPrincipal.build(usuario);
+    }
+}
