dbfc571b5179df0508fadc616eb9ae3ffb1c4428
Diana Villarreal <dianacarolinav97@gmail.com>
2023-03-31 09:56:55 -0500

    Travel

---
+++ frontend/src/components/NavBarOnlyLogo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NavBarOnlyLogo = () => {
+    return (
+        <nav className='flex p-10 w-full h-24 items-center justify-end'>
+            <img className='w-44 h-44' src={logoPerfil} alt="" />
+
+
+
+        </nav>
+    );
+};
+
+export default NavBarOnlyLogo;
---
+++ frontend/src/pages/TravelPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBarOnlyLogo from '../components/NavBarOnlyLogo';
+
+const TravelPage = () => {
+    return (
+        <main className='bg-black'>
+            <NavBarOnlyLogo />
+            asdasdas
+        </main>
+    );
+};
+
+export default TravelPage;
