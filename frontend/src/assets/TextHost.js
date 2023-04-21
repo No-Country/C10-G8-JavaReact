1cd3f2caf13f79839c4aaf24d4f4860b07f9ae77
Diana Villarreal <dianacarolinav97@gmail.com>
2023-04-21 10:50:27 -0500

    lorem-mailto

---
+++ frontend/src/assets/TextHost.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+
+export default [
+    "Bienvenidos a este evento especial, hoy tenemos el placer de contar con un anfitrión excepcional. Nuestro anfitrión es conocido por su capacidad para crear ambientes cálidos y acogedores, y por su habilidad para hacer que todos se sientan cómodos y bienvenidos. Él es un experto en la organización de eventos y se ha encargado de cuidar todos los detalles para que hoy tengamos una experiencia inolvidable. Así que sin más preámbulos, les presento a nuestro anfitrión, quien será el encargado de guiarnos a través de esta maravillosa velada.",
+    "Este alojamiento es un lugar ideal para disfrutar de un merecido descanso en un ambiente tranquilo y acogedor. Sus habitaciones están diseñadas para ofrecer el máximo confort, con camas cómodas, ropa de cama de calidad y una decoración elegante y cuidada. Además, cuenta con todas las comodidades necesarias para hacer que su estancia sea perfecta, como televisión por cable, acceso a internet y aire acondicionado. En cuanto a las zonas comunes, el alojamiento dispone de espacios amplios y luminosos donde podrás relajarte y disfrutar de momentos de tranquilidad. También cuenta con servicios como un restaurante de alta calidad, un gimnasio y una piscina al aire libre para que puedas disfrutar al máximo de tu tiempo libre. En resumen, este alojamiento es la opción perfecta para aquellos que buscan un lugar acogedor y confortable donde disfrutar de su tiempo libre.",
+    
+]
